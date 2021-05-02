3834a957824274b51b11bb379a591679bb75d777
TheLokin <diego.ramil.lopez@udc.es>
2021-05-02 20:13:50 +0200

    SRResNet implementation using SSIM loss

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import sys
 import csv
 import torch
 import argparse
+import pytorch_ssim
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.utils as utils
@@ -20,7 +21,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--dataset", type=str, metavar="N",
                     help="Folder with the dataset images.")
 parser.add_argument("--architecture", type=int, metavar="N",
-                    help="The architecture to use: 1. SRResnet. 2. SRRGAN.")
+                    help="The architecture to use: 1. SRResnet (MSE). 2. SRRGAN. 3. SRResnet (SSIM).")
 parser.add_argument("--crop-size", type=int, default=200, metavar="N",
                     help="Crop size for the training images (default: 200).")
 parser.add_argument("--upscale-factor", type=int, default=2, metavar="N",
@@ -32,10 +33,10 @@ opt = parser.parse_args()
 target_size = opt.crop_size * opt.upscale_factor
 
 # Create the necessary folders
-for path in [os.path.join("weight", "SRResNet"),
-             os.path.join("output", "SRResNet"),
+for path in [os.path.join("output"),
+             os.path.join("weight", "SRResNet", "MSE"),
              os.path.join("weight", "SRGAN"),
-             os.path.join("output", "SRGAN")]:
+             os.path.join("weight", "SRResNet", "SSIM")]:
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -50,7 +51,7 @@ else:
 dataset = TrainDatasetFromFolder(opt.dataset, target_size, opt.upscale_factor)
 dataloader = DataLoader(dataset, pin_memory=True)
 
-# SRResNet
+# SRResNet (MSE)
 if opt.architecture == 1:
     # Construct network architecture model of generator
     netG = Generator(16, opt.upscale_factor).to(device)
@@ -65,13 +66,13 @@ if opt.architecture == 1:
     optimizer = optim.Adam(netG.parameters())
 
     # Loading SRResNet checkpoint
-    print("[*] Start training SRResNet model.")
+    print("[*] Start training SRResNet (MSE) model.")
     checkpoint = load_checkpoint(netG, optimizer, os.path.join(
-        "weight", "SRResNet", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
+        "weight", "SRResNet", "MSE", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
 
     # Create SRResNet log
     if checkpoint == 0:
-        with open(os.path.join("weight", "SRResNet", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "w+") as file:
+        with open(os.path.join("weight", "SRResNet", "MSE", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "w+") as file:
             writer = csv.writer(file)
             writer.writerow(["Epoch", "MSE Loss"])
 
@@ -109,24 +110,24 @@ if opt.architecture == 1:
             total_iter = i + (epoch - 1) * len(dataloader)
             if (total_iter + 1) % 5000 == 0:
                 utils.save_image(lr, os.path.join(
-                    "output", "SRResNet", "SRResNet_" + str(total_iter + 1) + "_lr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_lr.bmp"))
                 utils.save_image(hr, os.path.join(
-                    "output", "SRResNet", "SRResNet_" + str(total_iter + 1) + "_hr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_hr.bmp"))
                 utils.save_image(sr, os.path.join(
-                    "output", "SRResNet", "SRResNet_" + str(total_iter + 1) + "_sr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_sr.bmp"))
 
         # The model is saved every epoch
         torch.save({"epoch": epoch,
                     "optimizer": optimizer.state_dict(),
                     "model": netG.state_dict()
-                    }, os.path.join("weight", "SRResNet", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
+                    }, os.path.join("weight", "SRResNet", "MSE", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
 
         # Save training log
-        with open(os.path.join("weight", "SRResNet", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "a+") as file:
+        with open(os.path.join("weight", "SRResNet", "MSE", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "a+") as file:
             writer = csv.writer(file)
             writer.writerow([epoch, avg_loss / len(dataloader)])
 
-    print("[*] Training SRResNet model done!")
+    print("[*] Training SRResNet (MSE) model done!")
 
 # SRGAN
 elif opt.architecture == 2:
@@ -246,11 +247,11 @@ elif opt.architecture == 2:
             total_iter = i + (epoch - 1) * len(dataloader)
             if (total_iter + 1) % 5000 == 0:
                 utils.save_image(lr, os.path.join(
-                    "output", "SRGAN", "SRGAN_" + str(total_iter + 1) + "_lr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_lr.bmp"))
                 utils.save_image(hr, os.path.join(
-                    "output", "SRGAN", "SRGAN_" + str(total_iter + 1) + "_hr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_hr.bmp"))
                 utils.save_image(sr, os.path.join(
-                    "output", "SRGAN", "SRGAN_" + str(total_iter + 1) + "_sr.bmp"))
+                    "output", "train_" + str(total_iter + 1) + "_sr.bmp"))
 
         # The models are saved every epoch
         torch.save({"epoch": epoch,
@@ -269,6 +270,84 @@ elif opt.architecture == 2:
                 [epoch, d_avg_loss / len(dataloader), g_avg_loss / len(dataloader)])
 
     print("[*] Training SRGAN model done!")
+
+# SRResNet (SSIM)
+elif opt.architecture == 3:
+    # Construct network architecture model of generator
+    netG = Generator(16, opt.upscale_factor).to(device)
+
+    # Set the model to training mode
+    netG.train()
+
+    # Loss SRResNet
+    ssim_criterion = pytorch_ssim.SSIM().to(device)
+
+    # Define SRResNet model optimizer
+    optimizer = optim.Adam(netG.parameters())
+
+    # Loading SRResNet checkpoint
+    print("[*] Start training SRResNet (SSIM) model.")
+    checkpoint = load_checkpoint(netG, optimizer, os.path.join(
+        "weight", "SRResNet", "SSIM", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
+
+    # Create SRResNet log
+    if checkpoint == 0:
+        with open(os.path.join("weight", "SRResNet", "SSIM", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "w+") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Epoch", "SSIM Loss"])
+
+    # Train generator using SSIM loss
+    for epoch in range(checkpoint + 1, opt.epoch + 1):
+        progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
+        avg_loss = 0
+
+        for i, (input, target) in progress_bar:
+            # Set generator gradients to zero
+            netG.zero_grad()
+
+            # Generate data
+            lr = input.to(device)
+            hr = target.to(device)
+
+            # Generating fake high resolution images from real low resolution images
+            sr = netG(lr)
+
+            # The SSIM of the generated fake high-resolution image and real high-resolution image is calculated
+            loss = -ssim_criterion(sr, hr)
+
+            # Calculate gradients for generator
+            loss.backward()
+
+            # Update generator weights
+            optimizer.step()
+
+            # Show loss
+            avg_loss -= loss.item()
+            progress_bar.set_description("[" + str(epoch) + "/" + str(opt.epoch) + "][" + str(
+                i + 1) + "/" + str(len(dataloader)) + "] SSIM Loss: {:.6f}".format(-loss.item()))
+
+            # An image is saved every 5000 iterations
+            total_iter = i + (epoch - 1) * len(dataloader)
+            if (total_iter + 1) % 5000 == 0:
+                utils.save_image(lr, os.path.join(
+                    "output", "train_" + str(total_iter + 1) + "_lr.bmp"))
+                utils.save_image(hr, os.path.join(
+                    "output", "train_" + str(total_iter + 1) + "_hr.bmp"))
+                utils.save_image(sr, os.path.join(
+                    "output", "train_" + str(total_iter + 1) + "_sr.bmp"))
+
+        # The model is saved every epoch
+        torch.save({"epoch": epoch,
+                    "optimizer": optimizer.state_dict(),
+                    "model": netG.state_dict()
+                    }, os.path.join("weight", "SRResNet", "SSIM", "SRResNet_" + str(opt.upscale_factor) + "x.pth"))
+
+        # Save training log
+        with open(os.path.join("weight", "SRResNet", "SSIM", "SRResNet_Loss_" + str(opt.upscale_factor) + "x.csv"), "a+") as file:
+            writer = csv.writer(file)
+            writer.writerow([epoch, avg_loss / len(dataloader)])
+
+    print("[*] Training SRResNet (SSIM) model done!")
 
 else:
     print("[!] Wrong architecture.")
